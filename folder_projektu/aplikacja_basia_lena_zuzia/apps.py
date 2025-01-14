208447f2ed02c19b6fc823349c3b78915bfbc031
lenazie8 <lziemlewska@st.swps.edu.pl>
2025-01-14 15:18:20 +0100

    commit1

---
+++ folder_projektu/aplikacja_basia_lena_zuzia/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AplikacjaBasiaLenaZuziaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'aplikacja_basia_lena_zuzia'
