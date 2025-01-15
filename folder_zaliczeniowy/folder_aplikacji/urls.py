20b7b8627922aa86208895402ad273e33b82bafb
sskwarek2003 <sskwarek@st.swps.edu.pl>
2025-01-15 15:36:55 +0100

    poprawione_ścieżki_widoki

--- folder_zaliczeniowy/folder_aplikacji/urls.py
+++ folder_zaliczeniowy/folder_aplikacji/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('logowanie/', views.logowanie, name='logowanie'),
+    path('logowanie/', auth_views.LoginView.as_view(template_name='folder_aplikacji/logowanie.html'), name='logowanie'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- folder_zaliczeniowy/folder_aplikacji/views.py
+++ folder_zaliczeniowy/folder_aplikacji/views.py
@@ -2,12 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Transakcja, CelOszczednosciowy, Powiadomienie
 
-# Widok dla strony głównej
 @login_required
 def dashboard(request):
     return render(request, 'folder_aplikacji/base.html')
 
 def logowanie(request):
-    return render(request, 'folder_aplikacji/login.html')
-
-###
+    return render(request, 'folder_aplikacji/logowanie.html')
